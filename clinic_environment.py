a016e332fb4cfe2464737fa79dd7708c075efdfb
Albert Han <yjhan96@gmail.com>
2025-02-08 20:07:38 -0800

    first commit

---
+++ clinic_environment.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+
+
+class Nurse:
+    def __init__(self):
+        pass
+
+
+class Patient:
+    def __init__(self):
+        pass
+
+
+class Clinic:
+    def __init__(self):
+        pass
+
+
+class ClinicEnv(gym.Env):
+    def __init__(self):
+        pass
