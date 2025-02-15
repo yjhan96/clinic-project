1dcbc76677ff8e473cee55790b7125d6488af76e
Albert Han <yjhan96@gmail.com>
2025-02-14 22:59:46 -0800

    Finished clinic env and added tests

--- clinic_environment.py
+++ clinic_environment.py
@@ -5,6 +5,8 @@ import numpy as np
 OPERATING_TIME = 15
 MAX_TREATMENT_TIME = 1000
 MAX_CAPACITY = 10
+MIN_REWARD = -1_000_000
+MINUTE_PER_STEP = 5
 
 
 class NurseStatus(IntEnum):
@@ -14,11 +16,12 @@ class NurseStatus(IntEnum):
 
 
 class Nurse:
-    def __init__(self, location: int):
-        self.location = location
+    def __init__(self, initial_clinic: "Clinic"):
+        self.location: "Clinic" = initial_clinic
         self.status: NurseStatus = NurseStatus.IDLE
-        self.operating_minutes_left = 0
-        self.traveling_minutes_left = 0
+        self.operating_minutes_left: int = 0
+        self.traveling_minutes_left: int = 0
+        self.treating_patient: "Patient" | None = None
 
     @staticmethod
     def get_observation_space(num_clinics: int) -> gym.spaces.Space:
@@ -32,10 +35,42 @@ class Nurse:
 
     def _get_obs(self):
         return {
-            "location": self.location,
+            "location": self.location.idx + 1,
             "operating_minutes_left": float(self.operating_minutes_left),
             "traveling_minutes_left": float(self.traveling_minutes_left),
         }
+
+    def treat(self, patient: "Patient"):
+        assert self.status == NurseStatus.IDLE
+        assert self.operating_minutes_left == 0
+        assert self.treating_patient is None
+
+        self.status = NurseStatus.IN_OPERATION
+        self.operating_minutes_left = OPERATING_TIME
+        self.treating_patient = patient
+
+    def travel_to(self, clinic: "Clinic", traveling_time: int):
+        assert self.status == NurseStatus.IDLE
+
+        self.status = NurseStatus.TRAVELING
+        self.traveling_minutes_left = traveling_time
+        self.location = clinic
+
+    def step(self, num_minutes):
+        match self.status:
+            case NurseStatus.IDLE:
+                return
+            case NurseStatus.IN_OPERATION:
+                self.operating_minutes_left -= num_minutes
+                if self.operating_minutes_left == 0:
+                    self.status = NurseStatus.IDLE
+                    self.treating_patient = None
+            case NurseStatus.TRAVELING:
+                self.traveling_minutes_left -= num_minutes
+                if self.traveling_minutes_left == 0:
+                    self.status = NurseStatus.IDLE
+            case _:
+                raise ValueError(f"Unknown nurse status: {self.status}")
 
 
 class PatientStatus(IntEnum):
@@ -50,6 +85,7 @@ class Patient:
         self.status: PatientStatus = PatientStatus.NOT_TREATED
         self.minutes_in_treatment: int = 0
         self.treated_at: "Clinic" | None = None
+        self.treating_nurse: Nurse | None = None
 
     @staticmethod
     def get_observation_space(num_clinics: int) -> gym.spaces.Space:
@@ -67,15 +103,70 @@ class Patient:
             "status": int(self.status),
             "treatment_time": float(self.treatmet_time),
             "minutes_in_treatment": float(self.minutes_in_treatment),
-            "treated_at": 0 if self.treated_at is None else self.treated_at.idx,
+            "treated_at": 0 if self.treated_at is None else self.treated_at.idx + 1,
         }
+
+    def get_treatment_from(self, nurse: Nurse):
+        if self.status == PatientStatus.NOT_TREATED:
+            self.status = PatientStatus.IN_TREATMENT
+            self.treating_nurse = nurse
+            self.treated_at = nurse.location
+            nurse.location.add_patient(self)
+        elif self.status == PatientStatus.IN_TREATMENT:
+            if self.treatmet_time - self.minutes_in_treatment == OPERATING_TIME:
+                if self.treated_at == nurse.location:
+                    self.treating_nurse = nurse
+                else:
+                    raise ValueError(
+                        f"Nurse location ({nurse.location}) is different from patient location ({self.treated_at})"
+                    )
+            else:
+                raise ValueError(
+                    f"patient treatment time is incorrect: {self.treatmet_time=}, {self.minutes_in_treatment=}"
+                )
+        else:
+            raise ValueError(f"Invalid status: {self.status}")
+
+    def needs_nurse(self) -> bool:
+        not_treated_yet = self.status == PatientStatus.NOT_TREATED
+        needs_last_treatment = (
+            self.status == PatientStatus.IN_TREATMENT
+            and self.treatmet_time - self.minutes_in_treatment == OPERATING_TIME
+        )
+        return not_treated_yet or needs_last_treatment
+
+    def step(self, time_in_minutes: int) -> bool:
+        match self.status:
+            case PatientStatus.NOT_TREATED:
+                return True
+            case PatientStatus.IN_TREATMENT:
+                if (
+                    self.minutes_in_treatment == self.treatmet_time - OPERATING_TIME
+                    and self.treating_nurse is None
+                ):
+                    return False
+
+                self.minutes_in_treatment += time_in_minutes
+                if self.minutes_in_treatment == OPERATING_TIME:
+                    self.treating_nurse = None
+                elif self.minutes_in_treatment == self.treatmet_time:
+                    self.status = PatientStatus.DONE
+                    self.treating_nurse = None
+                    clinic = self.treated_at
+                    self.treated_at = None
+                    clinic.remove_patient(self)
+                return True
+            case PatientStatus.DONE:
+                return True
+            case _:
+                raise ValueError(f"Invalid status: {self.status}")
 
 
 class Clinic:
-    def __init__(self, idx, capacity):
-        self.idx = idx
-        self.capacity = capacity
-        self.patients = []
+    def __init__(self, idx: int, capacity: int):
+        self.idx: int = idx
+        self.capacity: int = capacity
+        self.patients: list[Patient] = []
 
     @staticmethod
     def get_observation_space() -> gym.spaces.Space:
@@ -91,6 +182,19 @@ class Clinic:
             "capacity": float(self.capacity),
             "num_patients": float(len(self.patients)),
         }
+
+    def num_available_seats(self) -> int:
+        return self.capacity - len(self.patients)
+
+    def add_patient(self, patient: Patient):
+        if self.num_available_seats() == 0:
+            raise ValueError("Clinic is full!")
+        self.patients.append(patient)
+
+    def remove_patient(self, done_patient: Patient):
+        self.patients = [
+            patient for patient in self.patients if patient != done_patient
+        ]
 
 
 class ClinicEnv(gym.Env):
@@ -108,7 +212,7 @@ class ClinicEnv(gym.Env):
 
         self._reset()
 
-        self.observation_space = gym.spaces.Dict(
+        self._nonterminal_observation_space = gym.spaces.Dict(
             {
                 "nurses": gym.spaces.Tuple(
                     tuple(
@@ -131,34 +235,46 @@ class ClinicEnv(gym.Env):
                 ),
             }
         )
+        self._terminal_observation_space = gym.spaces.Discrete(1)
+        self.observation_space = gym.spaces.OneOf(
+            (self._nonterminal_observation_space, self._terminal_observation_space)
+        )
 
         self.action_space = gym.spaces.Tuple(
             tuple(
                 [
-                    gym.spaces.MultiDiscrete(
-                        [len(self.patients) + 1, len(self.clinics) + 1]
-                    )
+                    gym.spaces.Discrete(1 + len(self.patients) + len(self.clinics))
+                    for _ in range(len(self.nurses))
                 ]
             )
         )
 
     def _get_obs(self):
-        return {
-            "nurses": tuple([nurse._get_obs() for nurse in self.nurses]),
-            "patients": tuple([patient._get_obs() for patient in self.patients]),
-            "clinics": tuple([clinic._get_obs() for clinic in self.clinics]),
-        }
+        if not self.is_terminated:
+            return (
+                0,
+                {
+                    "nurses": tuple([nurse._get_obs() for nurse in self.nurses]),
+                    "patients": tuple(
+                        [patient._get_obs() for patient in self.patients]
+                    ),
+                    "clinics": tuple([clinic._get_obs() for clinic in self.clinics]),
+                },
+            )
+        else:
+            return (1, 0)
 
     def _get_info(self):
         return None
 
     def _reset(self):
-        self.nurses = [Nurse(0) for _ in range(self.num_nurses)]
-        self.patients = [Patient(patient_time) for patient_time in self.patient_times]
         self.clinics = [
-            Clinic(idx, capacity)
+            Clinic(idx[0], capacity)
             for idx, capacity in np.ndenumerate(self.clinic_capacity)
         ]
+        self.nurses = [Nurse(self.clinics[0]) for _ in range(self.num_nurses)]
+        self.patients = [Patient(patient_time) for patient_time in self.patient_times]
+        self.is_terminated = False
 
     def reset(self, seed: int | None = None, options: dict | None = None):
         super().reset(seed=seed)
@@ -167,3 +283,99 @@ class ClinicEnv(gym.Env):
         info = self._get_info()
 
         return obs, info
+
+    def is_valid_treatment(self, nurse: Nurse, patient: Patient) -> bool:
+        """Returns True iff nurse can treat patient at a valid time."""
+        # Nurse needs to be idle, and patient must seek for a nurse.
+        if not (nurse.status == NurseStatus.IDLE and patient.needs_nurse()):
+            return False
+
+        if patient.treated_at is not None and patient.treated_at != nurse.location:
+            return False
+
+        if patient.treated_at is None and nurse.location.num_available_seats() == 0:
+            return False
+
+        return True
+
+    def is_valid_travel(self, nurse: Nurse, clinic: Clinic) -> bool:
+        return nurse.status == NurseStatus.IDLE
+
+    def _get_terminal_obs(self):
+        return {
+            "nurses": tuple([nurse._get_terminal_obs() for nurse in self.nurses]),
+            "patients": tuple(
+                [patient._get_terminal_obs() for patient in self.patients]
+            ),
+            "clinics": tuple([clinic._get_terminal_obs() for clinic in self.clinics]),
+        }
+
+    def _get_terminal_state(self):
+        self.is_terminated = True
+        obs = self._get_obs()
+        reward = MIN_REWARD
+        terminated = True
+        truncated = False
+        info = self._get_info()
+
+        return obs, reward, terminated, truncated, info
+
+    def step(self, action: tuple[int]):
+        # Take action.
+        for nurse_idx, nurse_action in enumerate(action):
+            nurse = self.nurses[nurse_idx]
+
+            if nurse_action == 0:
+                pass
+            elif nurse_action >= 1 and nurse_action <= len(self.patients):
+                patient_idx = nurse_action - 1
+                patient_to_treat = self.patients[patient_idx]
+                if self.is_valid_treatment(nurse, patient_to_treat):
+                    patient_to_treat.get_treatment_from(nurse)
+                    nurse.treat(patient_to_treat)
+                else:
+                    return self._get_terminal_state()
+            else:
+                dest_clinic = nurse_action - len(self.patients) - 1
+                clinic = self.clinics[dest_clinic]
+                if self.is_valid_travel(nurse, clinic):
+                    nurse.travel_to(
+                        clinic, self.clinic_travel_times[nurse.location.idx][clinic.idx]
+                    )
+        # Step through time.
+
+        for nurse in self.nurses:
+            nurse.step(MINUTE_PER_STEP)
+
+        prev_num_done_patients = len(
+            [
+                patient
+                for patient in self.patients
+                if patient.status == PatientStatus.DONE
+            ]
+        )
+
+        sick_patients = []
+        for patient in self.patients:
+            is_patient_ok = patient.step(MINUTE_PER_STEP)
+            if not is_patient_ok:
+                sick_patients.append(patient)
+
+        if len(sick_patients) > 0:
+            return self._get_terminal_state()
+
+        curr_num_done_patients = len(
+            [
+                patient
+                for patient in self.patients
+                if patient.status == PatientStatus.DONE
+            ]
+        )
+
+        obs = self._get_obs()
+        info = self._get_info()
+        reward = curr_num_done_patients - prev_num_done_patients
+        terminated = curr_num_done_patients == len(self.patients)
+        truncated = False
+
+        return obs, reward, terminated, truncated, info
--- test_clinic.py
+++ test_clinic.py
@@ -3,7 +3,7 @@ import numpy as np
 from clinic_environment import ClinicEnv
 
 
-def test_obs():
+def get_test_env() -> ClinicEnv:
     clinic_capacity = np.array([1, 2])
     clinic_travel_times = np.array([[0, 10], [10, 0]])
     patient_times = np.array([30, 40])
@@ -14,9 +14,14 @@ def test_obs():
         patient_times=patient_times,
         num_nurses=num_nurses,
     )
+    return clinic_env
 
-    obs, info = clinic_env.reset()
+
+def test_obs():
+    clinic_env = get_test_env()
+    (state, obs), info = clinic_env.reset()
     assert info == None
+    assert state == 0
     assert sorted(list(obs.keys())) == ["clinics", "nurses", "patients"]
 
     assert obs["clinics"] == (
@@ -25,8 +30,8 @@ def test_obs():
     )
 
     assert obs["nurses"] == (
-        {"location": 0, "operating_minutes_left": 0.0, "traveling_minutes_left": 0.0},
-        {"location": 0, "operating_minutes_left": 0.0, "traveling_minutes_left": 0.0},
+        {"location": 1, "operating_minutes_left": 0.0, "traveling_minutes_left": 0.0},
+        {"location": 1, "operating_minutes_left": 0.0, "traveling_minutes_left": 0.0},
     )
 
     assert obs["patients"] == (
@@ -43,3 +48,146 @@ def test_obs():
             "treated_at": 0,
         },
     )
+
+
+def test_basic():
+    clinic_env = get_test_env()
+
+    (state, obs), reward, terminated, truncated, _ = clinic_env.step((1, 4))
+    assert state == 0
+    nurse_states = obs["nurses"]
+    assert nurse_states[0] == {
+        "location": 1,
+        "operating_minutes_left": 10.0,
+        "traveling_minutes_left": 0.0,
+    }
+    assert nurse_states[1] == {
+        "location": 2,
+        "operating_minutes_left": 0.0,
+        "traveling_minutes_left": 5.0,
+    }
+
+    clinic_states = obs["clinics"]
+    assert clinic_states[0] == {"capacity": 1.0, "num_patients": 1.0}
+
+    patient_states = obs["patients"]
+    assert patient_states[0] == {
+        "status": 2,
+        "treatment_time": 30.0,
+        "minutes_in_treatment": 5.0,
+        "treated_at": 1,
+    }
+    assert reward == 0
+    assert not terminated and not truncated
+
+    (_, obs), reward, _, _, _ = clinic_env.step((0, 0))
+    nurse_states = obs["nurses"]
+    assert nurse_states[0] == {
+        "location": 1,
+        "operating_minutes_left": 5.0,
+        "traveling_minutes_left": 0.0,
+    }
+    assert nurse_states[1] == {
+        "location": 2,
+        "operating_minutes_left": 0.0,
+        "traveling_minutes_left": 0.0,
+    }
+    patient_states = obs["patients"]
+    assert patient_states[0] == {
+        "status": 2,
+        "treatment_time": 30.0,
+        "minutes_in_treatment": 10.0,
+        "treated_at": 1,
+    }
+    assert reward == 0
+
+    (_, obs), reward, _, _, _ = clinic_env.step((0, 2))
+    nurse_states = obs["nurses"]
+    assert nurse_states[0] == {
+        "location": 1,
+        "operating_minutes_left": 0.0,
+        "traveling_minutes_left": 0.0,
+    }
+    assert nurse_states[1] == {
+        "location": 2,
+        "operating_minutes_left": 10.0,
+        "traveling_minutes_left": 0.0,
+    }
+
+    clinic_states = obs["clinics"]
+    assert clinic_states[0] == {"capacity": 1.0, "num_patients": 1.0}
+    assert clinic_states[1] == {"capacity": 2.0, "num_patients": 1.0}
+
+    patient_states = obs["patients"]
+    assert patient_states[0] == {
+        "status": 2,
+        "treatment_time": 30.0,
+        "minutes_in_treatment": 15.0,
+        "treated_at": 1,
+    }
+    assert patient_states[1] == {
+        "status": 2,
+        "treatment_time": 40.0,
+        "minutes_in_treatment": 5.0,
+        "treated_at": 2,
+    }
+    assert reward == 0
+
+    (_, obs), reward, _, _, _ = clinic_env.step((1, 0))
+    nurse_states = obs["nurses"]
+    assert nurse_states[0] == {
+        "location": 1,
+        "operating_minutes_left": 10.0,
+        "traveling_minutes_left": 0.0,
+    }
+    patient_states = obs["patients"]
+    assert patient_states[0] == {
+        "status": 2,
+        "treatment_time": 30.0,
+        "minutes_in_treatment": 20.0,
+        "treated_at": 1,
+    }
+
+    clinic_env.step((0, 0))
+    (_, obs), reward, _, _, _ = clinic_env.step((0, 0))
+
+    nurse_states = obs["nurses"]
+    assert nurse_states[0] == {
+        "location": 1,
+        "operating_minutes_left": 0.0,
+        "traveling_minutes_left": 0.0,
+    }
+
+    clinic_states = obs["clinics"]
+    assert clinic_states[0] == {"capacity": 1.0, "num_patients": 0.0}
+
+    patient_states = obs["patients"]
+    assert patient_states[0] == {
+        "status": 3,
+        "treatment_time": 30.0,
+        "minutes_in_treatment": 30.0,
+        "treated_at": 0,
+    }
+    assert patient_states[1] == {
+        "status": 2,
+        "treatment_time": 40.0,
+        "minutes_in_treatment": 20.0,
+        "treated_at": 2,
+    }
+    assert reward == 1
+
+    clinic_env.step((0, 0))
+    (_, obs), reward, _, _, _ = clinic_env.step((0, 2))
+
+    patient_states = obs["patients"]
+    assert patient_states[1] == {
+        "status": 2,
+        "treatment_time": 40.0,
+        "minutes_in_treatment": 30.0,
+        "treated_at": 2,
+    }
+
+    clinic_env.step((0, 0))
+    (_, obs), reward, terminated, _, _ = clinic_env.step((0, 0))
+    assert reward == 1
+    assert terminated
